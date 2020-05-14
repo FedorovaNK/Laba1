3dd3a743d4862f88d8c0e8c27eb5b025fc4b4f0d
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-05-14 20:49:57 +0300

    Add files via upload

---
+++ WebS-Laba_1/SharedComponents/TorgObjects.BusinessLogic/DomainObjects/Ports/IPechatProductsRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace TorgObjects.DomainObjects.Ports
+{
+    public interface IReadOnlyPechatProductsRepository
+    {
+        Task<PechatProducts> GetPechatProducts(long id);
+
+        Task<IEnumerable<PechatProducts>> GetAllPechatProductss();
+
+        Task<IEnumerable<PechatProducts>> QueryPechatProductss(ICriteria<PechatProducts> criteria);
+
+    }
+
+    public interface IPechatProductsRepository
+    {
+        Task AddPechatProducts(PechatProducts PechatProducts);
+
+        Task RemovePechatProducts(PechatProducts PechatProducts);
+
+        Task UpdatePechatProducts(PechatProducts PechatProducts);
+    }
+}
